873cac4423d69a39f68375e8d725410c50d1522f
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-08-18 12:40:58 +0100

    Update ok.cc

--- ok.cc
+++ ok.cc
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class B {
+	
+};
 
 class A {
 public:
 	A() :ptr(nullptr) {};
-	A(A* ptr) :ptr(ptr) {};
+	A(B* ptr) :ptr(ptr) {};
 	A(int* ptr) :int_ptr(ptr) {};
 	
-	A* ptr;
+	B* ptr;
 	int* int_ptr; 
 
 };
@@ -18,11 +21,14 @@ int main() {
 	int *c = new int(3);
 	
 	A *z = new A;
-	A *a = new A(c);
+	
+	A *a = new B(c);
 	A *b = new A(a);
 	A *w = new A(b);
 
 	delete a;
+	
+	delete c; 
 
   	cout << b->ptr << endl;
 	
